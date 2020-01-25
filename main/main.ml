b57be0f4b7b37d29b9974dca3d0d1dc1727f7f59
Alain Armand <aarmand.inbox@gmail.com>
2020-01-24 19:11:24 -0500

    Library "lib" not found.

--- main/main.ml
+++ main/main.ml
@@ -1,2 +1,10 @@
 let () =
   print_endline "Hello, world!"
+
+open Lib
+
+let () =
+  let result = Math.add 2 3 in
+  print_endline (string_of_int result);
+  let result = Math.sub 3 1 in
+  print_endline (string_of_int result)
